1623a806a53a0932ba46a4e9a96815534c9969e9
Mike.Bao <halo343gs@gmail.com>
2014-10-16 22:21:48 -0700

    server error client display

--- javascript/home.js
+++ javascript/home.js
@@ -1,1 +1,17 @@
 var rocketelo = angular.module('rocketelo', ['ui.utils']);
+
+rocketelo.controller('MainBodyController', function($scope) {
+  var flashMessage = "";
+
+  $scope.getFlashMessage = function() {
+    return flashMessage;
+  }
+  
+  $scope.setFlashMessage = function(newMessage) {
+    flashMessage = newMessage;
+  }
+
+  $scope.clearFlashMessage = function() {
+    flashMessage = "";
+  }
+});
--- javascript/login/login.js
+++ javascript/login/login.js
@@ -1,7 +1,7 @@
 rocketelo.controller('LoginController', function($scope, $http, $window) {
   $scope.loginSucceed = true;
   $scope.usernameError = false;
-
+  
   // LOGIN FUNCTIONALITY
   $scope.login = function(user) {
     $http.post('/login', {"username": user.username, "password": user.password}).
@@ -15,7 +15,7 @@ rocketelo.controller('LoginController', function($scope, $http, $window) {
         retObj = angular.fromJson(data);
         // Notify user that login failed. Only way login fails is if either something we awry on the server or the username/password don't match.
         if (retObj.ErrorCode == 6) {
-          // Unknown error on the server :(
+          $scope.setFlashMessage("Unknown error. Please try again.");
         } else {
           $scope.loginSucceed = false; 
         }
@@ -36,7 +36,7 @@ rocketelo.controller('LoginController', function($scope, $http, $window) {
         console.log("on error register " + data);
         retObj = angular.fromJson(data);
         if (retObj.ErrorCode == 6) {
-          // Unknown error on the server :(
+          $scope.setFlashMessage("Unknown error. Please try again.");
         } else if (retObj.ErrorCode == 1){
           $scope.usernameError = true; 
         }
