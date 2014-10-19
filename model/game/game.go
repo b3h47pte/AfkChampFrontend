a85a9d46d2974f8542163358359025239ba1fd0f
Mike.Bao <halo343gs@gmail.com>
2014-10-19 00:31:17 -0700

    list of games supported

--- model/game/game.go
+++ model/game/game.go
@@ -12,30 +12,28 @@ type GameRow struct {
   GameId int
   GameName string
   // Used in the URL
-  GameShortName string
+  GameShorthand string
 }
 
 // 'GetGames' returns a portion of the available games. We are provided with a range of games to get assuming the games
 // are sorted by game ID (number index).
 func GetGames(offset int, count int) ([]GameRow, error) {
-  requestGames := make([]GameRow, count, count)
+  requestGames := make([]GameRow, 0, 0)
   
   // Find the games
-  rows, err := model.Database.Queryx("SELECT * FROM games LIMIT ?, ?", offset, count)
+  rows, err := model.Database.Queryx("SELECT * FROM games")
   if err != nil {
     return nil, err
   }
   
   // Create the GameRow structs and then return it
-  objectCount := 0
   for rows.Next() {
     newObj := GameRow{}
     err = rows.StructScan(&newObj)
     if err != nil {
       continue
     }
-    requestGames[objectCount] = newObj
-    objectCount++
+    requestGames = append(requestGames, newObj)
   }
   
   return requestGames, nil
